f0296b85a8c813eed7c6cb27b2069d0ede973744
Matthew Carter <matthew@macarter.net>
2018-11-19 21:42:46 -0800

    Add champion data functionality along with static champion file

---
+++ modules/champions.py
@@ -0,0 +1,141 @@
+import discord
+from discord.ext import commands
+import json
+from riotwatcher import RiotWatcher
+import time
+
+# Load the config file
+with open('config.json') as json_data_file:
+    data = json.load(json_data_file)
+
+# Store config details
+key = data['key']
+
+watcher = RiotWatcher(key)
+default_region = 'na1'
+
+# Load the config file
+with open('data/static_champions.json') as json_data_file:
+    champions = json.load(json_data_file)
+
+
+class Champions:
+    def __init__(self, bot):
+        self.bot = bot
+        update_champions()
+
+    @commands.command(name='mastery')
+    async def get_champion_mastery(self, ctx, name: str, champ: str = None):
+        """Returns a players champion mastery details"""
+
+        summoner = watcher.summoner.by_name(default_region, name)
+        summoner_id = summoner['id']
+
+        if champ is None:
+            mastery = watcher.champion_mastery.by_summoner(default_region, summoner_id)
+
+            ret = ''
+
+            for i in range(0, 5):
+                champ_id = mastery[i].get('championId')
+
+                champ_name = get_champ_name(str(champ_id))
+                champ_level = mastery[i].get('championLevel')
+                champ_points = mastery[i].get('championPoints')
+
+                ret += 'Champion: ' + champ_name + '\n'
+                ret += '\tLevel: ' + str(champ_level) + '\n'
+                ret += '\tPoints: ' + str(champ_points) + '\n'
+
+        else:
+            champ_id = get_champ_id(champ)
+            mastery = watcher.champion_mastery.by_summoner_by_champion(default_region, summoner_id, champ_id)
+
+            champ_name = champ.lower().capitalize()
+            champ_level = mastery.get('championLevel')
+            champ_points = mastery.get('championPoints')
+
+            ret = 'Champion: ' + champ_name + '\n'
+            ret += '\tLevel: ' + str(champ_level) + '\n'
+            ret += '\tPoints: ' + str(champ_points)
+
+        await ctx.send(ret)
+
+    @commands.command(name='champion')
+    async def get_champion(self, ctx, name: str):
+        """Returns information on a champion"""
+
+        name = name.lower()
+        name = name.capitalize()
+
+        champ = 'Name: ' + champions['data'][name]['name'] + '\n'
+        champ += '\tTitle: ' + champions['data'][name]['title'] + '\n'
+        champ += '\tBlurb: ' + champions['data'][name]['blurb']
+
+        await ctx.send(champ)
+
+    @commands.command(name='rotation')
+    async def get_champion_rotation(self, ctx):
+        """Returns the current champion rotation"""
+
+        rotation = watcher.champion.rotations(default_region)
+
+        free_champ_ids = rotation['freeChampionIds']
+        free_champ_ids_new_players = rotation['freeChampionIdsForNewPlayers']
+        max_new_player_level = rotation['maxNewPlayerLevel']
+
+        free_champs = 'Free Champions: '
+        free_champs_new_players = 'Free Champions for New Players: '
+
+        for i in range(0, len(free_champ_ids)):
+            if i < len(free_champ_ids) - 1:
+                free_champs += get_champ_name(str(free_champ_ids[i])) + ', '
+            else:
+                free_champs += get_champ_name(str(free_champ_ids[i]))
+
+        for i in range(0, len(free_champ_ids_new_players)):
+            if i < len(free_champ_ids_new_players) - 1:
+                free_champs_new_players += get_champ_name(str(free_champ_ids_new_players[i])) + ', '
+            else:
+                free_champs_new_players += get_champ_name(str(free_champ_ids_new_players[i]))
+
+        ret = free_champs + '\n\n'
+        ret += free_champs_new_players + '\n\n'
+        ret += 'Max New Player level: ' + str(max_new_player_level)
+
+        await ctx.send(ret)
+
+
+def update_champions():
+    version_data = watcher.data_dragon.versions_for_region(default_region)
+
+    current_version = int(version_data.get('v').replace('.', ''))
+    version = int(champions['version'].replace('.', ''))
+
+    if current_version > version:
+        champs = watcher.data_dragon.champions(version_data.get('v'))
+
+        with open('data/static_champions.json', 'w') as outfile:
+            json.dump(champs, outfile)
+
+        print('Updated static_champions file to latest version')
+
+
+def get_champ_id(name: str):
+    name = name.lower()
+    name = name.capitalize()
+
+    champ_id = champions['data'][name]['key']
+
+    return int(champ_id)
+
+
+def get_champ_name(champ_id: str):
+    for champ in champions['data']:
+        champ_key = champions['data'][champ]['key']
+        if champ_key == champ_id:
+            return champ
+
+
+def setup(bot):
+    bot.add_cog(Champions(bot))
--- modules/summoners.py
+++ modules/summoners.py
@@ -1,4 +1,4 @@
-# TODO Use DDragon to pull champion data and parse that with champion-masteries-by-summoner
+# TODO Begin scouting report command
 
 import discord
 from discord.ext import commands
@@ -15,12 +15,6 @@ key = data['key']
 watcher = RiotWatcher(key)
 default_region = 'na1'
 
-version = watcher.data_dragon.versions_for_region(default_region)
-print(version)
-
-champions = watcher.data_dragon.champions(version.get('v'))
-print(champions.get('data'))
-
 
 class Summoners:
     def __init__(self, bot):
@@ -31,11 +25,30 @@ class Summoners:
         """Returns information on a summoner"""
 
         summoner = watcher.summoner.by_name(default_region, name)
+        mastery_score = watcher.champion_mastery.scores_by_summoner(default_region, summoner['id'])
         ranked_stats = watcher.league.positions_by_summoner(default_region, summoner['id'])
+
+        solo_rank = 'N/A'
+        flex_rank = 'N/A'
+        threes_rank = 'N/A'
+
+        for stats in ranked_stats:
+            queue_type = stats.get('queueType')
+
+            if queue_type == 'RANKED_SOLO_5x5':
+                solo_rank = rank_to_string(stats)
+            elif queue_type == 'RANKED_FLEX_SR':
+                flex_rank = rank_to_string(stats)
+            else:
+                threes_rank = rank_to_string(stats)
 
         ret = 'Name: ' + summoner.get('name') + '\n'
         ret += '\tLevel: ' + str(summoner.get('summonerLevel')) + '\n'
-        ret += '\tRank: ' + rank_to_string(ranked_stats[0])
+        ret += '\tMastery Score: ' + str(mastery_score) + '\n'
+        ret += '\tRanked: \n'
+        ret += '\t\tSolos - ' + solo_rank + '\n'
+        ret += '\t\tFlex 5v5 - ' + flex_rank + '\n'
+        ret += '\t\tFlex 3v3 - ' + threes_rank
 
         await ctx.send(ret)
 
